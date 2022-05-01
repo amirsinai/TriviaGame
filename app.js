bc80579b6fcc9c25b00222307b51edf2d17c7919
amirsinai <amir.sinaei84@gmail.com>
2022-05-01 14:20:12 -0700

    Added array of questions, hover text is css and buttons for two mor games

---
+++ app.js
@@ -0,0 +1,27 @@
+// button1.addEventListener("click", console.log("destination question"));
+
+const TechQ = [
+  "What year was the very first model of the iPhone released?",
+  "What’s the shortcut for the “copy” function on most computers?",
+  "VWhat is often seen as the smallest unit of memory?",
+  "BIs Java a type of OS?",
+  "Who is often called the father of the computer?",
+  "What does “HTTP” stand for?",
+  "What is the name of the man who launched eBay back in 1995?",
+  "Which email service is owned by Microsoft?",
+  "Google Chrome, Safari, Firefox, and Explorer are different types of what?",
+  "What was Twitter’s original name?",
+];
+
+const TechA = [
+  "2007",
+  "ctrl c",
+  "kilobyte",
+  "No",
+  "Charles Babbage",
+  "HyperText Transfer Protocol",
+  "Pierre Omidyar",
+  "Hotmail",
+  "Web browsers",
+  "twttr",
+];
